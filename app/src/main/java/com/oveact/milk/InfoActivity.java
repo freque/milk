fbb1373dcbaf6adee9a81b8fb594c0c836488e33
hdrsd <ahnjun09@gmail.com>
2017-05-14 13:04:31 +0900

    Update Activities

---
+++ app/src/main/java/com/oveact/milk/InfoActivity.java
@@ -0,0 +1,20 @@
+package com.oveact.milk;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ahnju on 2017-05-14.
+ */
+
+public class InfoActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info
+        );
+
+    }
+
+}
---
+++ app/src/main/java/com/oveact/milk/RegisterActivity.java
@@ -0,0 +1,17 @@
+package com.oveact.milk;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ahnju on 2017-05-14.
+ */
+
+public class RegisterActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
