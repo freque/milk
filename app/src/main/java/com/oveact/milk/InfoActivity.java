4e8f290fa4df486363680d584cab6015ddc92082
hdrsd <ahnjun09@gmail.com>
2017-05-14 15:09:39 +0900

    Add Override and Add Custom Actionbar

--- app/src/main/java/com/oveact/milk/InfoActivity.java
+++ app/src/main/java/com/oveact/milk/InfoActivity.java
@@ -1,7 +1,13 @@
 package com.oveact.milk;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by ahnju on 2017-05-14.
@@ -9,12 +15,35 @@ import android.support.v7.app.AppCompatActivity;
 
 public class InfoActivity extends AppCompatActivity {
 
+    @Override
     protected void onCreate(Bundle savedInstanceState){
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info
-        );
+        setContentView(R.layout.activity_info);
+
+        setCustomActionbar();
 
     }
+
+    public void setCustomActionbar(){
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+
+        View cV = LayoutInflater.from(this).inflate(R.layout.actionbar_layout, null);
+        actionBar.setCustomView(cV);
+
+        Toolbar tb = (Toolbar) cV.getParent();
+        tb.setContentInsetsAbsolute(0, 0);
+
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.argb(255, 143, 202, 247)));
+
+        ActionBar.LayoutParams params = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT);
+        actionBar.setCustomView(cV, params);
+    }
+
 
 }
