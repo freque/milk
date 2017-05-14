5eda5facc0e9d69e9aa5e5b7632835b724d5dd8b
hdrsd <ahnjun09@gmail.com>
2017-05-14 12:49:46 +0900

    Initial commit

---
+++ app/src/main/java/com/oveact/milk/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.oveact.milk;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ahnju on 2017-05-14.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(false);
+
+        Handler hd = new Handler() {
+            public void handleMessage(Message msg) {
+                finish();
+            }
+        };
+        hd.sendEmptyMessageDelayed(0, 3000);
+
+    }
+
+}
